8dc8d59a216d7154989bb8fda1323cc1ec218d86
Adam Shannon <adamkshannon@gmail.com>
2016-12-06 15:23:49 -0600

    Try out mapRouteResultFuture
    This fails because RouteResult is already computed at this time.
    
    ```
    [error] x record timings properly when routes are completed with futures
    [error]  7.506277273708834E-5 is less than 0.09 (HttpTimerMetricsSpec.scala:95)
    ```

--- src/main/scala/HttpTimerMetrics.scala
+++ src/main/scala/HttpTimerMetrics.scala
@@ -2,6 +2,7 @@ package backline.http.metrics
 import com.codahale.metrics._
 import akka.http.scaladsl.server.{Directive0, RequestContext}
 import scala.util.control.NonFatal
+import scala.util.Failure
 
 trait HttpTimerMetrics extends MetricsBase {
   def timerDirective: Directive0 =
@@ -11,16 +12,22 @@ trait HttpTimerMetrics extends MetricsBase {
     timer(_ => name)
 
   private[this] def timer(nameFunc: RequestContext => String): Directive0 = {
-    mapInnerRoute { inner => ctx =>
-      val timer = findAndRegisterTimer(nameFunc(ctx)).time()
+    import scala.concurrent.ExecutionContext.Implicits.global
+    mapRouteResultFuture { fut =>
+      // nameFunc(fut)
+      val timer = findAndRegisterTimer("ping-fut.GET").time()
       try {
-        inner(ctx)
+        fut
       } catch {
         case NonFatal(err) =>
-          ctx.fail(err)
-      } finally {
-        timer.stop()
+          timer.stop()
+          Failure(err)
       }
+      fut.onComplete {
+        case _ =>
+          timer.stop()
+      }
+      fut
     }
   }
 }
