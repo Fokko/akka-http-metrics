147508c042434c5c5fd7556ae0e3ab8c3415f127
Adam Shannon <adamkshannon@gmail.com>
2017-02-03 11:04:54 -0600

    deprecate ResponseCodeMetrics

--- src/main/scala/ResponseCodeMetrics.scala
+++ src/main/scala/ResponseCodeMetrics.scala
@@ -6,9 +6,11 @@ import scala.concurrent.ExecutionContext
 trait ResponseCodeMetrics extends MetricsBase
     with HttpResponseWrapping {
 
+  @deprecated("`responseCodeMetrics` has been deprecated. Switch to `withStatusCodeCounter` in `StatusCodeMetrics` which doesn't require an ExecutionContext", "0.6.0")
   def responseCodeMetrics(implicit ec: ExecutionContext): Directive0 =
     responseCodes(ctx => getMetricName(ctx))
 
+  @deprecated("`responseCodeMetricsWithName` has been deprecated. Switch to `withStatusCodeCounterNamed` in `StatusCodeMetrics` which doesn't require an ExecutionContext", "0.6.0")
   def responseCodeMetricsWithName(name: String)(implicit ec: ExecutionContext): Directive0 =
     responseCodes(_ => name)
 }
