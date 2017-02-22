40c8ab78fcb109c19439653f2411bedc0052635c
Denis Mikhaylov <notxcain@gmail.com>
2017-02-22 17:17:07 +0300

    Add deprecation annotations to `StatusCodeMetrics`

--- src/main/scala/backline/http/metrics/StatusCodeMetrics.scala
+++ src/main/scala/backline/http/metrics/StatusCodeMetrics.scala
@@ -2,11 +2,20 @@ package backline.http.metrics
 
 import akka.http.scaladsl.server.Directive0
 
+@deprecated(
+  "`StatusCodeMetrics` has been deprecated. Use `StatusCodeCounterDirectives`.",
+  "0.6.0")
 trait StatusCodeMetrics extends MetricsBase with StatusCodeCounterCommon {
 
+  @deprecated(
+    "`withStatusCodeCounter` has been deprecated. Use `StatusCodeCounterDirectives#withStatusCodeCounter`.",
+    "0.6.0")
   def withStatusCodeCounter: Directive0 =
     responseCodes(ctx => getMetricName(ctx))
 
+  @deprecated(
+    "`withStatusCodeCounterNamed` has been deprecated. Use `StatusCodeCounterDirectives#withStatusCodeCounterNamed`.",
+    "0.6.0")
   def withStatusCodeCounterNamed(name: String): Directive0 =
     responseCodes(_ => name)
 }
