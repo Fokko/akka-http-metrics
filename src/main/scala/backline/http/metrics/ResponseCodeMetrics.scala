d3ff42e19d7ca48443c167b4aedc964196f6d54b
Adam Shannon <adamkshannon@gmail.com>
2017-05-08 15:24:21 -0500

    restore resp code metrics

---
+++ src/main/scala/backline/http/metrics/ResponseCodeMetrics.scala
@@ -0,0 +1,16 @@
+package backline.http.metrics
+
+import akka.http.scaladsl.server.Directive0
+import scala.concurrent.ExecutionContext
+
+trait ResponseCodeMetrics extends MetricsBase
+    with StatusCodeCounterCommon {
+
+  @deprecated("`responseCodeMetrics` has been deprecated. Switch to `withStatusCodeCounter` in `StatusCodeMetrics` which doesn't require an ExecutionContext", "0.6.0")
+  def responseCodeMetrics(implicit ec: ExecutionContext): Directive0 =
+    responseCodes(ctx => getMetricName(ctx))
+
+  @deprecated("`responseCodeMetricsWithName` has been deprecated. Switch to `withStatusCodeCounterNamed` in `StatusCodeMetrics` which doesn't require an ExecutionContext", "0.6.0")
+  def responseCodeMetricsWithName(name: String)(implicit ec: ExecutionContext): Directive0 =
+    responseCodes(_ => name)
+}
